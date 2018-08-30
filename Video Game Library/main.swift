f30da3e471c6a07c026a16e84f5faf121c7acb9c
Jacob Finn <jacobfinn@macbook-pro-9.local>
2018-08-30 09:28:59 -0500

    Initial Commit

---
+++ Video Game Library/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Video Game Library
+//
+//  Created by Jacob Finn on 8/30/18.
+//  Copyright © 2018 Jacob Finn. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
